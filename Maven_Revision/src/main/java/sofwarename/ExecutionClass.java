36bfdf14b3ff0d7b5c5c43dbc7d6ea62a6c3bed2
Jai Ganesh <Jai Ganesh@DESKTOP-OGSNO18>
2024-08-11 13:18:17 +0530

    Initial Commit

---
+++ Maven_Revision/src/main/java/sofwarename/ExecutionClass.java
@@ -0,0 +1,14 @@
+package sofwarename;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ExecutionClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver web = new ChromeDriver();
+		web.get("https://learn-automation.com/");
+	}
+
+}
